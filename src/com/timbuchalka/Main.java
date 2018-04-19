802a51991ca7b5e997b4da3de14aa5204828e4e9
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-19 13:36:00 +0200

    arrays

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -1,8 +1,66 @@
 package com.timbuchalka;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+//        arrays();
+        arraysInputs();
+    }
+
+    private static void arrays() {
+
+//        int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+        int[] myIntArray = new int[10];
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+
+        double[] myDoubleArray = new double[10];
+
+        printArray(myIntArray);
+    }
+
+    public static void printArray(int[] array) {
+
+        for(int i = 1; i < array.length; i++) {
+            System.out.println("element " + i + ", value is " + array[i]);
+        }
+    }
+
+    private static void arraysInputs() {
+
+        int[] myIntegers = getIntegers(5);
+
+        for (int i = 0; i<myIntegers.length; i++) {
+            System.out.println("element " + i + ", typed value was " + myIntegers[i]);
+        }
+
+        System.out.println("avg is " + getAverage(myIntegers));
+    }
+
+    private static int[] getIntegers(int number) {
+
+        System.out.println("Enter " + number + "int values.\r");
+        int[] values = new int[number];
+
+        for(int i = 0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for(int i = 0; i<array.length; i++) {
+            sum += array[i];
+        }
+
+        return (double) sum/ (double) array.length;
     }
 }
