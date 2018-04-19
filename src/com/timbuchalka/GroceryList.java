85af8fbd74175ff1232c13b57e43867189f720db
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-19 15:15:36 +0200

    list and arrayList part 2

--- src/com/timbuchalka/GroceryList.java
+++ src/com/timbuchalka/GroceryList.java
@@ -13,14 +13,14 @@ public class GroceryList {
 
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
-        for (int i = 0; i<groceryList.size(); i++) {
-            System.out.println((i+1) + ". " + groceryList.get(i));
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
         }
     }
 
     public void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
-        System.out.println("grocery item " + (position+1) + " has been modified.");
+        System.out.println("grocery item " + (position + 1) + " has been modified.");
     }
 
     public void removeGroceryItem(int position) {
@@ -28,4 +28,15 @@ public class GroceryList {
         groceryList.remove(position);
     }
 
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+
+        int position = groceryList.indexOf(searchItem);
+
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+
+        return null;
+    }
 }
